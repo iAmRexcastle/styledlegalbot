ac152fe59b02c2b6ec5a4ee3855dc0037cb0758a
Ronnie Sandlin <ronniesandlin@Mac.hsd1.tn.comcast.net>
2025-02-20 05:38:11 -0600

    Remove BubbleBackground component

--- src/app/components/BubbleBackground.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-
-export default function AnimatedGradientBackground() {
-  // Covers entire viewport behind content
-  return <div className="absolute inset-0 -z-10 gradient-background" />;
-}
